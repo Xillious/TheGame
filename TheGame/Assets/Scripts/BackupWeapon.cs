64b346c7fd0dfbcfbe6f4e94d7ed78aa2e9d9c9b
Xillious <jamesphypers@gmail.com>
2019-10-15 15:23:43 +1100

    added more characters fixed tutorial levels

---
+++ TheGame/Assets/Scripts/BackupWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackupWeapon : MonoBehaviour
+{
+    public GameObject backupWeapon;
+    private PlayerController playerController;
+
+    void Start()
+    {
+       
+        playerController = FindObjectOfType<PlayerController>();
+
+        if (playerController.myWeapon == null)
+        {
+            Debug.Log("Player has no weapon");
+            Instantiate(backupWeapon, transform.position, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
